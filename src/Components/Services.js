39a81bdb77939e97804d8c599c32161d01dfaf81
CliffVille <Cliffordvill17@gmail.com>
2025-01-06 01:41:06 -0800

    Finished content on the Services component. Need to make it responsive yet

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -7,18 +7,43 @@ const Services = () => {
         <h1 className="text-5xl text-white font-kumbh font-extrabold pb-4">Our Services</h1>
         <p className="text-white w-2/4">Our goal is to help you minimize your taxes and put you in a competitive position to succeed in your business. Your growth is our growth! Email or call us today.</p>
       </div>
-      <div className="h-[250px]">
+      <div className="h-[250px] flex flex-col justify-center items-center">
         <div className="flex flex-row mb-2">
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">T1 Personal Tax Returns</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">T2 Corporate Tax Returns</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">Book Keeping</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">Trust Returns</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
         </div>
+    
         <div className="flex flex-row">
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
-          <div className="w-[250px] h-[70px] bg-black rounded-md mr-4"></div>
-          <div className="w-[250px] h-[70px] bg-black rounded-md"></div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">Film Tax Credits</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">Appeals to CRA</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">Informal Appeals to Tax Court</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
+          <div className="relative w-[250px] h-[70px] rounded-md mr-4">
+            <div className="w-full h-full bg-black bg-imageThree bg-cover rounded-md text-white text-center pt-6">PST & GST Returns</div>
+            <div className="absolute inset-0 bg-black opacity-10 rounded-md"></div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        kumbh: ['Kumbh Sans', 'sans-serif'],
+      backgroundImage: {
+        'imageOne': "url('./assets/image.svg')",
+        'imageTwo': "url('./assets/imageTwo.svg')",
+        'imageThree': "url('./assets/imageThree.svg')",
+        'imageFour': "url('./assets/imageFour.svg')",
+        'imageFive': "url('./assets/imageFive.svg')",
+        'imageSix': "url('./assets/imageSix.svg')",
+      },
+        fontFamily: {
+          kumbh: ['Kumbh Sans', 'sans-serif'],
       }
     },
   },
