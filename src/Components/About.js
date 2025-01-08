5e406c9e0c4b50eec6ceb74911e25878b54684ed
CliffVille <Cliffordvill17@gmail.com>
2025-01-08 02:03:31 -0800

    Added a footer

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { MdPhone, MdEmail } from 'react-icons/md';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import profile from '../assets/profile.png'
 
 const About = () => {
   return (
-    <div className="bg-[#101010] w-full min-h-screen h-[500px] flex justify-center items-center" id="about">
+    <div className="bg-[#101010] w-full min-h-screen h-[500px] flex justify-center items-center pb-16" id="about">
       <div className="flex flex-col items-center w-3/4 mt-4">
         <h1 className="text-7xl text-white font-kumbh font-extrabold mt-4 mb-4">Our Team</h1>
         <div className="w-1/2 flex justify-center">
@@ -15,6 +17,29 @@ const About = () => {
         </div>
         <div className="max-w-7xl flex justify-center items-center h-auto mb-4">
           <p className="w-[75%] text-[16px] font-kumbh text-white">Over 28 years accounting and taxation experience. An undergraduate degree in business and a Master&apos;s in Business Administration, worked at Ernst &amp; Young LLP Vancouver for 5 years and moved to Buckley Dodds LLP in 2002 as a Staff Accountant and became Tax Principal. Specializes in field of taxation, PST, GST, Personal, Corporate and Trust Income Tax Return. Also specializes in specialized taxation in the field of Film Tax Credit for productions and Scientific Research and Experimental Developmental Tax Credit</p>
+        </div>
+        <div className="my-6 mx-auto lg:my-8 max-w-7xl w-[75%] flex justify-between items-center">
+          <div className="flex">
+            <p className="text-white text-4xl">Want to connect?</p>
+          </div>
+          <div className="flex flex-row space-x-4">
+            <div className="flex flex-col items-center">
+              <a href="tel:+17789862589" className="text-white text-2xl flex items-center">
+                <MdPhone className="mr-2 mt-1" />
+              </a>
+              <a href="mailto:dgo@simplitax.ca" className="text-white text-2xl flex items-center">
+                <MdEmail className="mr-2 mt-1" />
+              </a>
+            </div>
+            <div className="flex flex-col">
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white text-2xl flex items-center mt-1">
+                <FaFacebook />
+              </a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white text-2xl flex-items-center mt-1">
+                <FaInstagram />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+   
+        <footer className="w-full bg-[#434242]">
+
+            <div className="w-[57%] max-w-screen-xl mx-auto md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="#" className="flex items-center mb-4 sm:mb-0 ml-2">
+                        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="#" className="hover:underline">SimpliTax™</a> All Rights Reserved.</span>
+                    </a>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-6">Services</a>
+                        </li>
+                        <li>
+                            <a href="#" className="hover:underline me-4 md:me-2">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+
   )
 }
 
-export default Footer;
+export default Footer
